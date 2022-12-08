c9c8b0b2bcf6d5fa3e0471a3265c8cd2caef7a3a
Dmytro Mishchuk <dmi@adorsys.com.ua>
2022-12-08 15:18:50 +0200

    test

--- src/main/java/com/example/testapp/TestAppApplication.java
+++ src/main/java/com/example/testapp/TestAppApplication.java
@@ -1,13 +1,21 @@
 package com.example.testapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
 
+@Slf4j
 @SpringBootApplication
-public class TestAppApplication {
+public class TestAppApplication implements ApplicationListener<ApplicationReadyEvent> {
 
     public static void main(String[] args) {
         SpringApplication.run(TestAppApplication.class, args);
     }
 
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        log.info("APP STARTED!");
+    }
 }
